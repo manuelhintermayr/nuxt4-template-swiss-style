9e129df9032e888df1f56b97d6af139c0c0aed79
Manuel Hintermayr <manuel@hintermayr.net>
2025-09-07 22:16:41 +0200

    finished implementing date picker

--- app/components/FormsAdvancedExample.vue
+++ app/components/FormsAdvancedExample.vue
@@ -53,7 +53,7 @@
                             </UButton>
 
                             <template #content>
-                                <UCalendar v-model="selectedPickerDate" class="p-2" />
+                                <UCalendar v-model="selectedPickerDate" class="p-2 yolo" />
                             </template>
                         </UPopover>
                     </div>
